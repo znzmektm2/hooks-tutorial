1c5a2cd506ee711c47bcfcafb4bcce02d73d9c5e
znzmektm2 <tingcber2@naver.com>
2020-05-23 02:17:51 +0900

    add: useState, useEffect, useReducer, useMemo, useCallBack, useRef

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import Counter from "./Counter";
+import Info from "./Info";
+import Average from "./Average";
+
+const App = () => {
+  const [visible, setVisible] = useState(false);
+  return (
+    <div>
+      <Counter />
+      <br />
+      <button
+        onClick={() => {
+          setVisible(!visible);
+        }}
+      >
+        {visible ? "숨기기" : "보이기"}
+      </button>
+      <hr />
+      {visible && <Info />}
+      <br />
+      <Average />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Info.js
@@ -0,0 +1,77 @@
+import React, { useState, useEffect, useReducer } from "react";
+import useInputs from "./useInputs";
+
+function reducer(state, action) {
+  return {
+    ...state,
+    [action.name]: action.value,
+  };
+}
+const Info = () => {
+  // const [name, setName] = useState("");
+  // const [nickname, setNickname] = useState("");
+
+  // useEffect(() => {
+  //   //console.log("렌더링이 완료되었습니다!");
+  //   // console.log({
+  //   //   name,
+  //   //   nickname,
+  //   // });
+  //   console.log("effect");
+  //   console.log(name);
+  //   return () => {
+  //     console.log("cleanup");
+  //     console.log(name);
+  //   };
+  // }, [name]);
+
+  ///////////////////
+
+  // const onChangeName = (e) => {
+  //   setName(e.target.value);
+  // };
+
+  // const onChangeNickname = (e) => {
+  //   setNickname(e.target.value);
+  // };
+
+  ///////////////////
+
+  // const [state, dispatch] = useReducer(reducer, {
+  //   name: "",
+  //   nickname: "",
+  // });
+
+  // const { name, nickname } = state;
+  // const onChange = (e) => {
+  //   dispatch(e.target);
+  // };
+
+  const [state, onChange] = useInputs({
+    name: "",
+    nickname: "",
+  });
+  const { name, nickname } = state;
+
+  return (
+    <div>
+      <div>
+        {/* <input value={name} onChange={onChangeName} />
+        <input value={nickname} onChange={onChangeNickname} /> */}
+
+        <input name="name" value={name} onChange={onChange} />
+        <input name="nickname" value={nickname} onChange={onChange} />
+      </div>
+      <div>
+        <div>
+          <b>이름:</b> {name}
+        </div>
+        <div>
+          <b>닉네임:</b> {nickname}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Info;
